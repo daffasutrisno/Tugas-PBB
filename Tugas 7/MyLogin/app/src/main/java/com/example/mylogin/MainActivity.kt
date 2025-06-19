75095254948ced113d1062138cc56a2afe7a5f0c
daffasutrisno <daffarizky2004@gmail.com>
2025-06-19 10:50:49 +0700

    Commit Task 7

---
+++ Tugas 7/MyLogin/app/src/main/java/com/example/mylogin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mylogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.mylogin.ui.theme.LoginUITheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginUITheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
